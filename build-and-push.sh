4caaf19aaa408cbfdebada84ae9a56ff63b9067b
Oriol Rius <oriol@joor.net>
2022-08-17 16:25:32 +0200

    latest tag added

--- build-and-push.sh
+++ build-and-push.sh
@@ -6,22 +6,31 @@ do
   then
     SOFTETHER_REPOSITORY="https://github.com/SoftEtherVPN/SoftEtherVPN_Stable.git"
     SOFTETHER_VERSION="v4.39-9772-beta"
+    LATEST=true
   else
     SOFTETHER_REPOSITORY="https://github.com/SoftEtherVPN/SoftEtherVPN.git"
     SOFTETHER_VERSION="5.02.5180"
+    LATEST=false
   fi
 
-  #docker buildx prune -f
+  #sudo docker buildx prune -f
 
   for MODE in vpnclient vpnserver
   do
-    docker buildx build \
+    LATEST_TAG=""
+    if [ "$LATEST" = true ]; 
+    then 
+      LATEST_TAG="--tag oriolrius/softether-${MODE}:latest"
+    fi
+
+    sudo docker buildx build \
       --platform linux/amd64,linux/arm64 \
       --build-arg SOFTETHER_VERSION="${SOFTETHER_VERSION}" \
       --build-arg SOFTETHER_REPOSITORY="${SOFTETHER_REPOSITORY}" \
       --build-arg MODE="${MODE}" \
       --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
       --tag oriolrius/softether-${MODE}:${SOFTETHER_VERSION} \
+       \
       --push \
       .
   done
